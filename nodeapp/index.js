dfa908020561c9c9aefaaba0c9d8cfd9234d0a5b
jomasim <simjoms@gmail.com>
2019-11-15 11:31:59 +0300

    enable cors

---
+++ nodeapp/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var cors = require('cors');
+var app = express();
+
+app.use(cors);
+
+app.get('/', function(req, res){
+    res.send({ Oranges:10, Mangoes:14, Tomatoes:20, Grapes:23 });
+});
+
+app.listen('8080', function(){
+    console.log('Server is running on port 8080');
+})
