210c3189dad7008509eda13cfb97f0f0120c109a
jomasim <simjoms@gmail.com>
2019-11-18 11:30:33 +0300

    consumed node api

--- nodeapp/index.js
+++ nodeapp/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var cors = require('cors');
 var app = express();
 
-app.use(cors);
+app.use(cors())
 
 app.get('/', function(req, res){
     res.send({ Oranges:10, Mangoes:14, Tomatoes:20, Grapes:23 });
--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends React.Component{
 
@@ -13,23 +14,30 @@ class App extends React.Component{
     this.getFruits(); 
   }
 
-  getFruits() {
-    fetch("http://127.0.0.1:8080/").then((resp)=> resp.json())
-    .then((data) =>{
-    console.log("atleast we are here");
-    console.log("response", data);
-    this.setState({fruits:data});
-    return data;
-  });
+  getFruits = () => {
+    console.log('atleast we are here');
+    axios.get("http://127.0.0.1:8081/").then((response)=>{
+      if(response){
+        const fruits = response.data;
+        const mapped = Object.entries(fruits).map((item)=>{
+         return { name:item[0], available:item[1]};
+        });
+        this.setState({fruits: mapped});
+      }
+    })
+  
   }
   render(){
+    const { fruits } = this.state;
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hello we here to salute you!
-          </p>
+          <ul>
+            {fruits.map(fruit => 
+              <li key={fruit.name}>{fruit.name}</li>
+            )}
+          </ul>
         </header>
       </div>
     );
